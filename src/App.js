d97da055091cbc2d61adc0d4a553d1d9439e3a00
HamaHs <lusuy1995@gmail.com>
2024-01-16 12:21:08 +0100

    the project is collected

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
-import "./App.css";
+import { useState } from "react";
+import styles from "./App.module.css";
+import Button from "./components/Button";
+import Title from "./components/Title";
 
 function App() {
-   return <div className="App"></div>;
+   const buttons = [
+      { name: "decrease", sign: "-" },
+      { name: "reset", sign: 0 },
+      { name: "increase", sign: "+" },
+   ];
+   const [number, setNumber] = useState(0);
+
+   const updateNumber = (sign) => {
+      switch (sign) {
+         case "+":
+            setNumber((prevNumber) => prevNumber + 1);
+            break;
+         case "-":
+            setNumber((prevNumber) => prevNumber - 1);
+            break;
+         default:
+            setNumber(sign);
+            break;
+      }
+   };
+
+   const setNumberStyle = (num) => {
+      if (num > 0) {
+         return "increase";
+      } else if (num < 0) {
+         return "decrease";
+      }
+
+      return "default";
+   };
+
+   return (
+      <div className={styles.app}>
+         <div className={styles.appWrapper}>
+            <Title text="Counter" />
+
+            <div className={`${styles.number} ${styles[setNumberStyle(number)]}`}>{number}</div>
+
+            <div className={styles.btnContainer}>
+               {buttons.map((btn, index) => (
+                  <Button onClick={() => updateNumber(btn.sign)} key={index}>
+                     {btn.name}
+                  </Button>
+               ))}
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default App;
